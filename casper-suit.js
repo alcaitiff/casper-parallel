4ae27aa7ada3aa91b425413cc983ded600e2b570
Alan Rabello <alan@debian>
2015-09-08 21:23:13 -0300

    Now works

--- casper-suit.js
+++ casper-suit.js
@@ -4,6 +4,8 @@ var colors = require('colors');
 var fs = require('fs-tools');
 var mkdirp = require('mkdirp');
 var testCaseName = 'MyFirstTestCase';
+var path = require('path');
+var lib = path.resolve(__dirname, 'lib');
 console.log('Installing casper-suit'.blue.bold);
 createDir();
 
@@ -31,16 +33,20 @@ function createFiles() {
 
 function createRunner() {
   console.log('Creating the runner');
-  fs.copy('lib/run.sh', 'casper-suit/run.sh',
-    function() {
-      console.log('Runner created'.green);
+  fs.copy(lib + '/run.sh', 'casper-suit/run.sh',
+    function(err) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Runner created'.green);
+      }
     }
   );
 }
 
 function createServerConf() {
   console.log('Creating the server configuration file');
-  fs.copy('lib/serverConf.json', 'casper-suit/serverConf.json',
+  fs.copy(lib + '/serverConf.json', 'casper-suit/serverConf.json',
     function() {
       console.log('Server configuration file created'.green);
     }
@@ -49,17 +55,17 @@ function createServerConf() {
 
 function createFunctions() {
   console.log('Creating the functions files');
-  fs.copy('lib/screenshot.js', 'casper-suit/screenshot.js',
+  fs.copy(lib + '/screenshot.js', 'casper-suit/screenshot.js',
     function() {
       console.log('Screenshot file created'.green);
     }
   );
-  fs.copy('lib/lib.js', 'casper-suit/lib.js',
+  fs.copy(lib + '/lib.js', 'casper-suit/lib.js',
     function() {
       console.log('Lib file created'.green);
     }
   );
-  fs.copy('lib/conf.js', 'casper-suit/conf.js',
+  fs.copy(lib + '/conf.js', 'casper-suit/conf.js',
     function() {
       console.log('Casper configuration file created'.green);
     }
@@ -81,18 +87,19 @@ function createTestCase() {
 }
 
 function createTestCaseFiles() {
-  fs.copy('lib/map.js', 'casper-suit/' + testCaseName + '/map.js',
+  fs.copy(lib + '/map.js', 'casper-suit/' + testCaseName + '/map.js',
     function() {
       console.log('Map file created'.green);
     }
   );
-  fs.copy('lib/myfirsttest.js', 'casper-suit/' + testCaseName + '/myfirsttest.js',
+  fs.copy(lib + '/myfirsttest.js', 'casper-suit/' + testCaseName + '/myfirsttest.js',
     function() {
       console.log('Test file created'.green);
     }
   );
 }
 exports.createProject = function() {
+  //console.log(lib);
   //For test purposes
   createDir();
 };
